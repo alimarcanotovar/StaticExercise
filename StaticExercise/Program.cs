27c213aaf41d5870ea45dc592a424740c943f79f
alimarcanotovar <aliymarcanotovar91@gmail.com>
2022-08-22 11:50:18 -0500

    inital commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"What is your temp");
+            var userinput = int.Parse(Console.ReadLine());
+           var celcius = TempConveter.FahrenheitToCelsius(userinput);
+
+
+            Console.WriteLine($"celcius {celcius}");
+
+
+            Console.WriteLine($"what is your temp");
+            var userinput1 = int.Parse(Console.ReadLine());
+
+            var fahrenheit = TempConveter.CelsiusToFahrenhiet(userinput1);
+
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+        }
+
+
+    }
+}
---
+++ StaticExercise/TempConveter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    public static class TempConveter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            var result = (fahrenheit - 32) / 1.8;
+
+            return result;
+        }
+        public static double CelsiusToFahrenhiet(double celcius)
+        {
+            var results = (celcius * 9) / 5 + 32;
+            return results;
+        }
+    }
+}
+
